5cf736de8871c12eca2d7185c8f892b084ef08ab
maryhojati <maryhojati@gmail.com>
2024-04-29 18:35:25 +0330

    primary code with error: can not handle multiple query rows

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"strconv"
 	"time"
@@ -14,22 +15,55 @@ import (
 
 //Db on hard---->slow
 //important: conn db --->close
-func pg_conn(id int64) string {
+func pg_conn(lat, lng float64) string {
 	//cache redis ---->ram
 	//TODO
 	//hard
-	connStr := "user=postgres dbname=dB2 password=246 sslmode=disable" //connection string
-	db, err := sql.Open("postgres", connStr)                           //app---->pg
+	connStr := "user=postgres dbname=postgres password=246 sslmode=disable" //connection string
+	db, err := sql.Open("postgres", connStr)                                //app---->pg
 	if err != nil {
 		panic(err)
 	}
 
-	var name sql.NullString
-	if err := db.QueryRow("SELECT name FROM student WHERE id = $1", id).Scan(&name); err != nil {
-		panic(err)
+	query := `
+	SELECT
+	name
+    --,osm_id, amenity,    
+	,ST_Distance(ST_Transform(way, 4326),ST_SetSRID(ST_MakePoint($1,$2)), 4326)) AS distance
+	FROM planet_osm_point
+	WHERE amenity = 'restaurant'
+    AND ST_DWithin(
+        ST_Transform(way, 4326),
+        ST_SetSRID(ST_MakePoint($1,$2), 4326),
+        1000
+    )
+	ORDER BY distance
+	limit 10;
 
-	}
-	return name.String
+	`
+	var name string
+	var distance float64
+	//$1 long $2 lat
+	err := db.QueryRow(query, lng, lat).Scan(&name, &distance)
+	fmt.Println(name, distance)
+	return name
+	//if err != nil {
+	//	panic(err)
+	//
+	//}
+	//defer rows.Close()
+	//
+	//for rows.Next() {
+	//	var name string
+	//	var distance float64
+	//	err = rows.Scan(&name, &distance)
+	//	if err != nil {
+	//		panic(err)
+	//	}
+	//	fmt.Println(name, distance)
+	//	//add distance
+	//}
+	//
 
 }
 
@@ -56,8 +90,8 @@ func main() {
 
 		if id != 0 {
 			c.JSON(http.StatusOK, gin.H{
-				"id":   id,
-				"name": pg_conn(id),
+				"id": id,
+				//"name": pg_conn(id),
 			})
 		} else {
 			c.JSON(http.StatusOK, gin.H{
@@ -66,5 +100,25 @@ func main() {
 		}
 
 	})
+
+	// Query string parameters are parsed using the existing underlying request object.
+	// The request responds to a url matching:  /welcome?firstname=Jane&lastname=Doe
+	r.GET("/point", func(c *gin.Context) {
+		lat, err1 := strconv.ParseFloat(c.Query("lat"), 64)
+		long, err2 := strconv.ParseFloat(c.Query("long"), 64) // shortcut for c.Request.URL.Query().Get("lastname")
+		fmt.Print(lat, long)
+		if err1 == nil || err2 == nil {
+			c.JSON(http.StatusOK, gin.H{
+				"restaurant name": pg_conn(lat, long),
+			})
+		} else {
+			c.JSON(http.StatusOK, gin.H{
+				"res": "lat or Long Not found",
+			})
+		}
+
+		//c.String(http.StatusOK, "Hello %s %s", firstname, lastname)
+	})
+
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
